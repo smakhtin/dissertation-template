e112424a8f2b8403ba9e7c8c2009276e7a965116
Vadim Smakhtin <vadim@sovereignnature.com>
2026-05-24 15:41:59 +0200

    Big update: Figures for full page images

--- filters/fullpage-images.lua
+++ filters/fullpage-images.lua
@@ -1,12 +1,34 @@
 -- Lua filter to convert images with height=100% to full-page images
 
+local function escape_latex(text)
+  if not text or text == "" then
+    return ""
+  end
+
+  local escaped = text
+  escaped = escaped:gsub("\\", "\\textbackslash{}")
+  escaped = escaped:gsub("([{}$&#_%%])", "\\%1")
+  escaped = escaped:gsub("~", "\\textasciitilde{}")
+  escaped = escaped:gsub("%^", "\\textasciicircum{}")
+  return escaped
+end
+
+local function fullpage_latex(src, caption)
+  local safe_caption = escape_latex(caption)
+  if safe_caption == "" then
+    return string.format("\\fullpageimage{%s}", src)
+  end
+  return string.format("\\fullpageimage[%s]{%s}", safe_caption, src)
+end
+
 function Para(elem)
   -- Check if this paragraph contains only an image with height=100%
   if #elem.content == 1 and elem.content[1].t == "Image" then
     local img = elem.content[1]
     if img.attributes.height == "100%" then
       -- Create raw LaTeX for full-page image as a block
-      local latex = string.format("\\fullpageimage{%s}", img.src)
+      local caption = pandoc.utils.stringify(img.caption)
+      local latex = fullpage_latex(img.src, caption)
       return pandoc.RawBlock('latex', latex)
     end
   end
@@ -21,7 +43,13 @@ function Figure(elem)
       local img = plain.content[1]
       if img.attributes.height == "100%" then
         -- Create raw LaTeX for full-page image as a block
-        local latex = string.format("\\fullpageimage{%s}", img.src)
+        local caption = ""
+        if elem.caption then
+          caption = pandoc.utils.stringify(elem.caption)
+        else
+          caption = pandoc.utils.stringify(img.caption)
+        end
+        local latex = fullpage_latex(img.src, caption)
         return pandoc.RawBlock('latex', latex)
       end
     end
